2857dcd4c4252521125ac4aab141838967cb64ec
pa284 <pa284@njit.edu>
2021-07-01 18:02:44 -0400

    my first commit

---
+++ styles/javascript.js
@@ -0,0 +1,34 @@
+function setCookie(cname, value, days){
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 *1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cname){
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++){
+        let c = ca[i];
+        while(c.charAt(0) === ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(name) === 0){
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(){
+    let user = getCookie("username");
+    if(user !== ""){
+        alert("Welcome again " + user);
+    }
+    else {
+        user = prompt("Please enter your name here:", "");
+        if(user !== "" && user != null){
+            setCookie("username", user, 365);
+        }
+    }
+}
